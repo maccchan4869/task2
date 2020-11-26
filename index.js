c0047869eaa9d156dd796dc6c48df5c95727836f
maccchan4869 <ymatsuo554@gmail.com>
2020-11-26 12:20:19 +0900

    Revert "指摘事項修正"
    This reverts commit b89f48670e70bb0f72d4afdc50c050771655a311.

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
-window.onload = () => {
-  /** @member {number} maxNum    - 表示する最大数 */
-  const maxNum = 99;
+/** @param {number} maxNum    - 表示する最大数 */
+const maxNum = 99;
 
-  /** @member {object} dispItem  - 表示する文字列 */
-  const dispItem = {
-    fizz: 'Fizz',
-    buzz: 'Buzz',
-    space: ' ',
-    error: '整数値を入力してください'
-  };
+/** @param {object} dispItem  - 表示する文字列 */
+const dispItem = {
+  fizz: 'Fizz',
+  buzz: 'Buzz',
+  space: ' ',
+  error: '整数値を入力してください'
+}
+
+/**
+ * 実行ボタン押下時の処理
+ */
+const onClickRun = () => {
+  // チェック条件パターン
+  const pattern = /^([1-9]\d*|0)$/;
+
+  const divAns = document.getElementById("divAns");
+  const fizz = document.getElementById("fizzNum").value;
+  const buzz = document.getElementById("buzzNum").value;
   
-  const btn = document.getElementById('btnRun');
+  // 子要素を削除
+  divAns.innerHTML = '';
 
-  btn.addEventListener('click', () => {
-  
-    const pattern = /^([1-9]\d*|0)$/;
-
-    const divAns = document.getElementById("divAns");
-    const fizz = document.getElementById("fizzNum").value;
-    const buzz = document.getElementById("buzzNum").value;
-  
-    // 子要素を削除
-    divAns.innerHTML = '';
-
-    if (fizz.match(pattern) && buzz.match(pattern)){
-      // チェック成功時のDOM生成処理
-      const fizzNum = Number(fizz);
-      const buzzNum = Number(buzz);
-      for (let i = 1; i <= maxNum; i++) {
-        let text = '';
-        if(i%fizzNum === 0){
-          text = text + dispItem.fizz;
-        }
-        if(i%buzzNum === 0){
-          text = text + dispItem.buzz;
-        }
-        if(text !== ''){
-          const newRow = document.createElement("div");
-          newRow.innerHTML = text + dispItem.space + i;
-          divAns.appendChild(newRow);
-        }
+  if (fizz.match(pattern) && buzz.match(pattern)){
+    // チェック成功時のDOM生成処理
+    const fizzNum = Number(fizz);
+    const buzzNum = Number(buzz);
+    for (let i = 1; i <= maxNum; i++) {
+      let text = '';
+      if(i%fizzNum === 0){
+        text = text + dispItem.fizz;
       }
-    } else {
-      // チェック失敗時のDOM生成処理
-      const newRow = document.createElement("div");
-      newRow.innerHTML = dispItem.error;
-      divAns.appendChild(newRow);
+      if(i%buzzNum === 0){
+        text = text + dispItem.buzz;
+      }
+      if(text !== ''){
+        const newRow = document.createElement("div");
+        newRow.innerHTML = text + dispItem.space + i;
+        divAns.appendChild(newRow);
+      }
     }
-  });
-};
+  } else {
+    // チェック失敗時のDOM生成処理
+    const newRow = document.createElement("div");
+    newRow.innerHTML = dispItem.error;
+    divAns.appendChild(newRow);
+  }
+}
